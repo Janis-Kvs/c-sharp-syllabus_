03ac9f3eabe1835e0d7ad343559bbc29221483de
Janis Kazjonovs <janis.kazjonovs@gmail.com>
2021-07-13 18:49:40 +0300

    Completed till ex4

--- csharp-basics/exercises/TypesAndVariables/Casting/Program.cs
+++ csharp-basics/exercises/TypesAndVariables/Casting/Program.cs
@@ -17,15 +17,16 @@ namespace Casting
 
         static void first()
         {
+            
             String a = "1";
             int b = 2;
             int c = 3;
             double d = 4;
             float e = 5;
 
-            //fixme - should be 15 :|
-            //int sum = a + b + c + d + e;
-            //Console.WriteLine(sum);
+            //fixme - should be 15 :| 
+            int sum = Convert.ToInt32(a) + b + c + (int)d + (int)e;
+            Console.WriteLine(sum);
         }
 
         static void second()
@@ -37,8 +38,8 @@ namespace Casting
             float e = 5.3f;
 
             //fixme - should be 15.5 :| 
-            //float sum = a + b + c + d + e;
-            //Console.WriteLine(sum);
+            float sum = Convert.ToSingle(a) + b + c + (float)d + e;
+            Console.WriteLine(sum);
         }
     }
 }
---
+++ csharp-basics/exercises/TypesAndVariables/Exercise4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Exercise4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            int birthYear;
+            Console.WriteLine("Enter name: ");
+            name = Console.ReadLine();
+            Console.WriteLine("enter year of birth: ");
+            int.TryParse(Console.ReadLine(), out birthYear);
+
+            Console.WriteLine($"My name is {name} and I was born on {birthYear}.");
+
+        }
+    }
+}
